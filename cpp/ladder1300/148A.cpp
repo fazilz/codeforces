0f158c0aa77d408a4e7388155f03de8fe3456427
Fazil Zuhaib <zuhaibfazil@gmail.com>
2021-02-26 00:30:18 -0500

    solved 148A, revist this later to use std algorithm

---
+++ cpp/ladder1300/148A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    // int k, l, m, n, d;
+    int n[5];
+    for_each(begin(n), end(n), [](int& e) {cin >> e;});
+    int count = 0;
+    for (int i = 1; i <= n[4]; i++)
+        if((i%n[0]==0 )|| (i%n[1]== 0) || (i%n[2]==0) || (i%n[3]==0))
+            count++;
+    cout << count << endl;
+    return 0;
+}
