95755a764e94b540e157b20d4986f301e79ef52b
Fazil Zuhaib <zuhaibfazil@gmail.com>
2021-04-26 18:26:31 -0400

    solved 118B

---
+++ cpp/ladder1300/118B.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+// Generates a pattern for handkercheif for given n
+// n is int[2, 9]
+// pattern(2) results in pattern below:
+//     0
+//   0 1 0
+// 0 1 2 1 0
+//   0 1 0
+//     0
+int main(){
+    int n;
+    cin >> n;
+    for (int row = -n; row <= n; ++row){
+        int mid = n - abs(row);
+        for (int col = 0; col < abs(row); ++col){
+            cout << " ";
+        }
+        for (int col = 0; col < mid; ++col){
+            cout << col << " ";
+        }
+        for (int col = mid; col > 0; --col){
+            cout << col << " ";
+        }
+        cout << 0<< endl;
+    }
+    return 0;
+}
